b274dd42da1c9d0f028b05cb01dfff6c0c286c29
Shae West <shreddedchickencreator@gmail.com>
2021-06-11 22:45:55 +1200

    Changed paragraph font size

--- src/assets/shelf/indexText.js
+++ src/assets/shelf/indexText.js
@@ -1,5 +1,5 @@
 const indexText = {
-    header:"Welcome to my portfolio website!",
+    header:"Welcome to my portfolio website, presented by Shae West!",
     paragraph1:"As you can tell, this website is currently a complete mess. I plan to update this website with quite a few features. First, you should"+
     "expect that this website will contain the following... 1: A portfolio page, which will be grabbed from github to display on here. 2: A "+
     "blog post page 3: A mini roadmap page, which will mention what projects I am currently working on, including the features. 4: A contact"+ 
--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Paragraph = styled.p`
     color:#f8f8ff;
+    font-size:20px;
 `
 
 export default Paragraph;
