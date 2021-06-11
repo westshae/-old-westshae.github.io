1681fa9fe0593a7b9b278e349224d7b460c8d1d4
Shae West <shreddedchickencreator@gmail.com>
2021-06-11 22:42:21 +1200

    Added a github calendar third party component to the index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,7 @@
-
 import React from "react";
 
 //Text imports
 import indexText from "../assets/shelf/indexText.js"
-
-//Image imports
-import pfp from "../assets/images/misc/pfp.jpg"
 
 //Component imports
 import Container from "../components/Container"
@@ -16,8 +12,16 @@ import Paragraph from "../components/Paragraph"
 import TextWrapper from "../components/TextWrapper"
 import Image from "../components/Image"
 
+//Third-party component imports
+import GitHubCalendar from "react-github-calendar";
 
 const MainPage = () => {
+    const theme = {
+        background: 'transparent',
+        text: '#f8f8ff',
+        grade0: '#1e2230',
+        
+      };
     return(
         <Container>
             <Navigation page="home"/>
@@ -31,6 +35,7 @@ const MainPage = () => {
                         {indexText.paragraph2}
                     </Paragraph>
                 </TextWrapper>
+                <GitHubCalendar username="westshae" theme={theme} blockSize={24} blockMargin={4} fontSize={20} showTotalCount={false} fullYear={false}/>
             </Content>
         </Container>
     );
