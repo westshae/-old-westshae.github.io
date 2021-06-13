c1afb086270039f59a87f98f8846f7887526a7db
Shae West <shreddedchickencreator@gmail.com>
2021-06-14 00:19:55 +1200

    Refactored Navigation component, with comments, removed unnecessary code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,57 +4,44 @@ import {useHistory} from "react-router-dom";
 import MediaQuery from "react-responsive";
 import React from "react"
 
-const NavButton = styled.button`
+//Style for navigation buttons
+const Button = styled.button`
     background:transparent;
     color:#f8f8ff;
     border:none;
-    text-align:center;
-    padding: 10px;
     height:3rem;
-    font-size:20px;
+    font-size:1.25em;
     font-family:Helvetica;
-    ${props => props.reverse && css`
+    ${props => props.right && css`
         margin-right:auto;
     `};
     :hover{
         background-color: #181b27;
     }
-    :active{
-        background-color:#1b1e2b;
-    }
 `
-
+//Style for navigation bar background
 const StyledNavigation = styled.nav`
     background-color:#1e2230;
-    border-bottom:0.5vh solid #1b1e2b;
+    border-bottom:0.25rem solid #1b1e2b;
     display:flex;
     height:3rem;
-    flex-direction:row;
-    
 `
 
+//Logic for navigation bar
 function Navigation(props){
 
     const history = useHistory();
 
-
-    const indexRoute=()=> {
-        let path = `index`;
-        history.push(path);
-    }
-
-    const projectsRoute=()=>{
-        let path = `projects`;
-        history.push(path);
-    }
+    const indexRoute=()=> {history.push(`index`);}
+    const projectsRoute=()=>{history.push(`projects`);}
 
     return (
         <StyledNavigation>
             <MediaQuery query="(min-device-width: 480px)">
-                <NavButton reverse main bold onClick={indexRoute}>/dev/altoyadev/{props.page}/</NavButton>
+                <Button right onClick={indexRoute}>/dev/altoyadev/{props.page}/</Button>
             </MediaQuery>
-            <NavButton main onClick={indexRoute}>Home</NavButton>
-            <NavButton main onClick={projectsRoute}>Projects</NavButton>
+            <Button onClick={indexRoute}>Home</Button>
+            <Button onClick={projectsRoute}>Projects</Button>
         </StyledNavigation>
     )
     
