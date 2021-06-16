9f2f13875084101d496bbb9c3b6ca465c3aab143
Shae West <shreddedchickencreator@gmail.com>
2021-06-16 23:41:05 +1200

    Updated content.jsx to be supportive of mobile

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,12 +3,18 @@ import styled from "styled-components";
 
 //Wraps all content on the page, excluding navbar
 const Content = styled.div`
-    margin-left:10%;
-    margin-right:10%;
+    margin-left:7.5rem;
+    margin-right:7.5rem;
     background-clip:margin-box;
     background-color:#272C3D;
     display:flex;
     flex-direction:column;
+    @media only screen 
+    and (max-device-width: 480px)
+    and (orientation: portrait) {
+        margin-left:1rem;
+        margin-right:1rem;
+    }
 
 `
 
