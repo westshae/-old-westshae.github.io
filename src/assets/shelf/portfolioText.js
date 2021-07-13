01c3b08eb07042371a0a679838360e041b00cdea
westshae <shaewest02@gmail.com>
2021-07-13 13:33:15 +1200

    Updated portfolioText to remove old 'L' system

--- src/assets/shelf/portfolioText.js
+++ src/assets/shelf/portfolioText.js
@@ -1,6 +1,6 @@
 const portfolioText = {
     header:"Portfolio projects.",
     paragraph1:"This page is to display all my projects that I have worked on since late 2019. To get more information about my projects, click on the header of each card,"+
-    " as that will send you to the github repo of that project, which all have a readme file. My largest projects can be seen via the 'L' at the end of the header, these will be at the top."
+    " as that will send you to the github repo of that project, which all have a readme file."
 }
 export default portfolioText;
